9abe9b1b366be33d2bd1c62530c687bcd2a7fd06
Deepa Joshi <msdipajoshi@gmail.com>
2022-12-02 14:35:24 +0545

    lazy loading in angular

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    DoctorComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
